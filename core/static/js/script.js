adcacb7e0af050f7d21447bce740937e7d4b7f62
John <jlennie1996@gmail.com>
2020-10-17 13:59:49 +0100

    Add files via upload

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -102,6 +102,29 @@ function build_results_table(data) {
 }
 
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Mon', 20, 28, 38, 45],
+        ['Tue', 31, 38, 55, 66],
+        ['Wed', 50, 55, 77, 80],
+        ['Thu', 77, 77, 66, 50],
+        ['Fri', 68, 66, 22, 15]
+      // Treat first row as data as well.
+    ], true);
+
+    var options = {
+        legend:'none'
+    };
+
+    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
+
+
 function start_trader(e, market_pair){
     e.preventDefault();
     
