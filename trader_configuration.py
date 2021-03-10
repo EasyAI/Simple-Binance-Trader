4870f5361e4b48da473229bb77c0a27784bd9473
John <jlennie1996@gmail.com>
2021-03-10 20:33:51 +0000

    Update trader_configuration.py

--- trader_configuration.py
+++ trader_configuration.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-
 import logging
 import numpy as np
 import technical_indicators as TI
@@ -21,6 +19,53 @@ def technical_indicators(candles):
 
     return(indicators)
 
+'''
+--- Current Supported Order ---
+    Below are the currently supported order types that can be placed which the trader
+
+-- MARKET --
+    To place a MARKET order you must pass:
+        'side'              : 'SELL', 
+        'description'       : 'Long exit signal', 
+        'order_type'        : 'MARKET'
+    
+-- LIMIT STOP LOSS --
+    To place a LIMIT STOP LOSS order you must pass:
+        'side'              : 'SELL', 
+        'price'             : price,
+        'stopPrice'         : stopPrice,
+        'description'       : 'Long exit stop-loss', 
+        'order_type'        : 'STOP_LOSS_LIMIT'
+
+-- LIMIT --
+    To place a LIMIT order you must pass:
+        'side'              : 'SELL', 
+        'price'             : price,
+        'description'       : 'Long exit stop-loss', 
+        'order_type'        : 'LIMIT'
+
+-- OCO LIMIT --
+    To place a OCO LIMIT order you must pass:
+        'side'              : 'SELL', 
+        'price'             : price,
+        'stopPrice'         : stopPrice,
+        'stopLimitPrice'    : stopLimitPrice,
+        'description'       : 'Long exit stop-loss', 
+        'order_type'        : 'OCO_LIMIT'
+
+--- Key Descriptions--- 
+    Section will give brief descript of what each order placement key is and how its used.
+        side            = The side the order is to be placed either buy or sell.
+        price           = Price for the order to be placed.
+        stopPrice       = Stop price to trigger limits.
+        stopLimitPrice  = Used for OCO to to determine price placement for part 2 of the order.
+        description     = A description for the order that can be used to identify multiple conditions.
+        order_type      = The type of the order that is to be placed.
+
+--- Candle Structure ---
+    Candles are structured in a multidimensional list as follows:
+        [[time, open, high, low, close, volume], ...]
+'''
 
 def other_conditions(custom_conditional_data, position_information, previous_trades, position_type, candles, indicators, symbol):
     can_order = True
@@ -34,6 +79,7 @@ def other_conditions(custom_conditional_data, position_information, previous_tra
 
 
 def long_exit_conditions(custom_conditional_data, trade_information, indicators, prices, candles, symbol):
+    ## Place Long exit (sell) conditions under this section.
     macd = indicators['macd']
 
     if macd[0]['macd'] < macd[1]['macd'] and macd[1]['hist'] < macd[0]['hist']:
@@ -57,6 +103,7 @@ def long_exit_conditions(custom_conditional_data, trade_information, indicators,
 
 
 def long_entry_conditions(custom_conditional_data, trade_information, indicators, prices, candles, symbol):
+    ## Place Long entry (buy) conditions under this section.
     macd = indicators['macd']
 
     if macd[0]['macd'] > macd[1]['macd'] and macd[1]['hist'] > macd[0]['hist']:
@@ -69,8 +116,10 @@ def long_entry_conditions(custom_conditional_data, trade_information, indicators
 
 
 def short_exit_conditions(custom_conditional_data, trade_information, indicators, prices, candles, symbol):
+    ## Place Short exit (sell) conditions under this section.
     pass
 
 
 def short_entry_conditions(custom_conditional_data, trade_information, indicators, prices, candles, symbol):
+    ## Place Short entry (buy) conditions under this section.
     pass
